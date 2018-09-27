ebd90caa4862e64b0b6b993d21243087b99be993
André Cruz <andre@cabine.org>
2018-09-27 12:17:14 +0100

    Added body parsing and CSRF protection

--- README.md
+++ README.md
@@ -3,14 +3,12 @@ Experimental starter pack for Node.JS webapps
 
 ## TODO
 1. Add authentication
-1. Sessions
 1. Add parameter validation (Joi, class-transformer/validator)
 1. Add Graphql sanity limits (DoS)
 1. File uploads
 1. Websockets
 1. i18n
 1. Tests
-1. security (CSRF)
 1. https://github.com/godaddy/terminus
 1. Dockerfile
 1. Helm package
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,20 @@
 import * as Router from "koa-router";
 import { logger } from "logger";
+import * as CSRF from "koa-csrf";
+import * as KoaBody from "koa-body";
 
 export const router = new Router();
+
+router.use(KoaBody({ multipart: true })).use(
+  new CSRF({
+    invalidSessionSecretMessage: "Invalid session secret",
+    invalidSessionSecretStatusCode: 403,
+    invalidTokenMessage: "Invalid CSRF token",
+    invalidTokenStatusCode: 403,
+    excludedMethods: ["GET", "HEAD", "OPTIONS"],
+    disableQuery: false,
+  }),
+);
 
 router.get("index", "/", async (ctx, next) => {
   if (ctx.isAuthenticated()) {
@@ -9,6 +22,16 @@ router.get("index", "/", async (ctx, next) => {
   } else {
     logger.debug("No authentication");
   }
-  await ctx.render('user')
-  // ctx.body = "Hello world";
+  await ctx.render("user");
+});
+
+router.get("auth-register", "/auth/register", async (ctx, next) => {
+  await ctx.render("register", {
+    register_url: router.url("auth-register-post"),
+    csrf: ctx.csrf,
+  });
+});
+
+router.post("auth-register-post", "/auth/register", async (ctx, next) => {
+  console.log("TEST");
 });
