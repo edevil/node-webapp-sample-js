54d870bf0c91840df622f53e7040cd6de9bffd2e
André Cruz <andre@cabine.org>
2018-10-02 13:50:33 +0100

    Add password confirmation

--- src/dtos/create-user.ts
+++ src/dtos/create-user.ts
@@ -1,4 +1,5 @@
 import { IsAlphanumeric, IsString, MaxLength, MinLength } from "class-validator";
+import { IsEqualTo } from "custom_validators";
 
 export class CreateUser {
   @IsString()
@@ -10,4 +11,9 @@ export class CreateUser {
   @IsString()
   @MinLength(8)
   password: string;
+
+  @IsEqualTo("password", {
+    message: "Password confirmation must equal password",
+  })
+  password_confirmation: string;
 }
