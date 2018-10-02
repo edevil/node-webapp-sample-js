f50a4436a89da44e6d4c5fd2328641222f9e04eb
André Cruz <andre@cabine.org>
2018-10-02 12:09:54 +0100

    Deal with already existing usernames

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,7 @@ import * as KoaBody from "koa-body";
 import { transformAndValidate } from "class-transformer-validator";
 import { CreateUser } from "dtos/create-user";
 import { createUser } from "service";
-import { getRepository } from "typeorm";
+import { getRepository, QueryFailedError } from "typeorm";
 import { User } from "entities/user";
 
 export const router = new Router();
@@ -40,6 +40,8 @@ router.get("auth-register", "/auth/register", async (ctx, next) => {
 });
 
 router.post("auth-register-post", "/auth/register", async (ctx, next) => {
+  // TODO check if user is already authenticated
+
   let createReq: CreateUser;
   try {
     createReq = <CreateUser>await transformAndValidate(CreateUser, ctx.request.body);
@@ -55,9 +57,21 @@ router.post("auth-register-post", "/auth/register", async (ctx, next) => {
     });
     return;
   }
-  logger.info(createReq.username);
-  // TODO deal with QueryFailedError: duplicate key value violates unique constraint "UQ_78a916df40e02a9deb1c4b75edb"
-  const user = await createUser(createReq, getRepository(User));
+
+  let user;
+  try {
+    user = await createUser(createReq, getRepository(User));
+  } catch (error) {
+    if (error instanceof QueryFailedError) {
+      logger.debug("Username already exists", { username: createReq.username });
+      await ctx.render("register", {
+        register_url: router.url("auth-register-post"),
+        csrf: ctx.csrf,
+        error: error,
+      });
+      return;
+    }
+  }
   logger.info("New user created", { userId: user.id });
   await ctx.login(user);
   ctx.redirect(router.url("auth-register"));
