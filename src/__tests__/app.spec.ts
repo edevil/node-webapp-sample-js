d1d035ac700fde32c177f27e9519582a724cd9ae
André Cruz <andre@cabine.org>
2018-10-04 15:05:16 +0100

    Continued testing

--- src/__tests__/app.spec.ts
+++ src/__tests__/app.spec.ts
@@ -1,10 +1,34 @@
 import { app } from "@app/app";
 import * as request from "supertest";
+import { router } from "@app/routes";
+import * as CSRF from "csrf";
 
 describe("GET / - simple test", () => {
   it("Hello API request", async () => {
-    const result = await request(app.callback()).get("/");
+    const result = await request(app.callback()).get(router.url("index"));
     expect(result.text).toContain("Sample node app");
     expect(result.status).toEqual(200);
+  });
+});
+
+describe("User registration tests", () => {
+  it("GET user registration page", async () => {
+    const result = await request(app.callback()).get(router.url("auth-register"));
+    expect(result.text).toContain("Sample node app");
+    expect(result.status).toEqual(200);
+  });
+
+  it("POST successful user registration", async () => {
+    CSRF.prototype.verify = jest.fn(() => true);
+    const username = "teste1";
+    const password = "teste12345";
+    const result = await request(app.callback())
+      .post(router.url("auth-register"))
+      .type("form")
+      .send({ username: username, password: password, password_confirmation: password })
+      .set("csrf-token", "test");
+    expect(result.text).toContain("Sample node app");
+    expect(result.status).toEqual(302);
+    expect(result.header["location"]).toEqual(router.url("index"));
   });
 });
