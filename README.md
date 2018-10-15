64043b960381a66740d9199ebf81c649d40e0797
André Cruz <andre@cabine.org>
2018-10-15 12:29:30 +0100

    JSON logs in production

--- README.md
+++ README.md
@@ -7,10 +7,11 @@ Experimental starter pack for Node.JS webapps
 1. Helm package
 1. CD Cloud Build script
 1. Accept X- headers (app.proxy = true)
-1. JSON logging in production
+1. stackdriver-friendly http context in request logs
 1. File uploads
 1. Websockets
 1. TSLint, tslint-config-prettier ?
+1. Include linter in CI
 1. Global error handler
 1. camelCase <-> snake_case
 
--- src/logger.ts
+++ src/logger.ts
@@ -1,10 +1,15 @@
 import * as winston from "winston";
 import { getContextStorage } from "@emartech/cls-adapter";
+import { format } from "winston";
+
+const addContextFormat = format((info, opts) => {
+  const allInfo = Object.assign(info, getContextStorage());
+  return allInfo;
+});
 
 const myFormat = winston.format.printf(info => {
   const message = `${info.timestamp} ${info.level}: ${info.message.trim()}`;
-  const allInfo = Object.assign(info, getContextStorage());
-  const extra = Object.keys(allInfo)
+  const extra = Object.keys(info)
     .filter(kname => !["message", "level", "timestamp"].includes(kname))
     .map(kname => `[${kname}: ${info[kname]}]`)
     .join(" ");
@@ -13,6 +18,10 @@ const myFormat = winston.format.printf(info => {
 
 export const logger = winston.createLogger({
   level: "debug",
-  format: winston.format.combine(winston.format.timestamp(), myFormat),
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    addContextFormat(),
+    process.env.NODE_ENV !== "production" ? myFormat : winston.format.json(),
+  ),
   transports: [new winston.transports.Console({ silent: process.env.NODE_ENV === "test" })],
 });
