22ce3ebe29722933d459eedbc4fe2bda1beef4e0
André Cruz <andre@cabine.org>
2018-10-15 16:10:44 +0100

    WIP on Helm package

--- README.md
+++ README.md
@@ -5,14 +5,16 @@ Experimental starter pack for Node.JS webapps
 ## TODO
 
 1. Helm package
-1. CD Cloud Build script `helm upgrade noder --namespace noder-develop noder --set image.tag=master_64043b960381a66740d9199ebf81c649d40e0797`
+1. CD Cloud Build script `helm upgrade noder --namespace noder-develop noder --timeout 60 --set image.tag=master_64043b960381a66740d9199ebf81c649d40e0797`
+1. Show ip in request logs
 1. Accept X- headers (app.proxy = true)
 1. stackdriver-friendly http context in request logs
+1. Redo request logger
 1. File uploads
 1. Websockets
 1. TSLint, tslint-config-prettier ?
 1. Include linter in CI
-1. Global error handler
+1. Global error handler (Sentry)
 1. camelCase <-> snake_case
 
 # Migrations
@@ -24,6 +26,12 @@ Experimental starter pack for Node.JS webapps
 ## Run migrations
 
     npm run typeorm -- migration:run
+
+### Postgres extensions needed
+
+1. uuid-ossp
+1. unaccent
+1. pg_trgm
 
 # Helm package
 
--- ormconfig.ts
+++ ormconfig.ts
@@ -3,14 +3,27 @@ import { Logger, QueryRunner } from "typeorm";
 import { logger } from "./src/logger";
 
 class MyCustomLogger implements Logger {
-  log(level: "log" | "info" | "warn", message: any, queryRunner?: QueryRunner): any {}
+  log(level: "log" | "info" | "warn", message: any, queryRunner?: QueryRunner): any {
+    let logFunc;
+    if (level === "log") {
+      logFunc = logger.debug;
+    } else if (level === "info") {
+      logFunc = logger.info;
+    } else if (level === "warn") {
+      logFunc = logger.warn;
+    }
+    logFunc("TypeORM log", { typeorm_message: message });
+  }
 
-  logMigration(message: string, queryRunner?: QueryRunner): any {}
+  logMigration(message: string, queryRunner?: QueryRunner): any {
+    logger.info("TypeORM migration", { typeorm_message: message });
+  }
 
   logQuery(query: string, parameters?: any[], queryRunner?: QueryRunner): any {
     if (config.logSQL) {
       const sql =
-        query + (parameters && parameters.length ? " -- PARAMETERS: " + MyCustomLogger.stringifyParams(parameters) : "");
+        query +
+        (parameters && parameters.length ? " -- PARAMETERS: " + MyCustomLogger.stringifyParams(parameters) : "");
       logger.debug("SQL", { query: sql });
     }
   }
@@ -27,7 +40,9 @@ class MyCustomLogger implements Logger {
     logger.warning("SQL SLOW", { query: sql, duration: time });
   }
 
-  logSchemaBuild(message: string, queryRunner?: QueryRunner): any {}
+  logSchemaBuild(message: string, queryRunner?: QueryRunner): any {
+    logger.info("TypeORM schema build", { typeorm_message: message });
+  }
 
   protected static stringifyParams(parameters: any[]) {
     try {
