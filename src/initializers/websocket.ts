6818c4423bb2e2d39b69be0ba5d0cc42c44eebc6
André Cruz <andre@cabine.org>
2018-10-22 14:24:46 +0100

    Verify JWT tokens sent by client

--- src/initializers/websocket.ts
+++ src/initializers/websocket.ts
@@ -1,5 +1,7 @@
+import * as jwt from "jsonwebtoken";
 import * as socketio from "socket.io";
 import * as redisAdapter from "socket.io-redis";
+import { config } from "../config";
 import { logger } from "../logger";
 import { getNewRedis } from "./redis";
 
@@ -17,21 +19,24 @@ export const initWebsocket = (server, app) => {
     logger.debug(`A client has connected`, { socketid: socket.id });
     socket.on("disconnect", () => logger.debug("A user has disconnected"));
     socket.on("chat message", msg => {
-      logger.debug(`Message: ${msg}`);
+      logger.debug(`Message: ${msg}`, { userId: socket.request.userId });
       io.emit("chat message", msg);
     });
   });
 
   io.use((socket, next) => {
-    let error = null;
+    const token = socket.handshake.query.token;
+    let data;
     try {
-      const ctx = app.createContext(socket.request, socket.request.res);
-      socket.request.session = ctx.session;
+      data = jwt.verify(token, config.appKeys[0]);
     } catch (err) {
-      logger.warn("Could not create context", { err });
-      error = err;
+      logger.debug("Could not validate token", { token });
+      return next();
     }
-    return next(error);
+
+    logger.debug("Decoded token", { data: JSON.stringify(data) });
+    socket.request.userId = data.userId;
+    return next();
   });
 };
 
