f2b148b3fe29994d627576669c4773c719ee4604
André Cruz <andre@cabine.org>
2018-10-29 10:17:58 +0000

    Updated dependencies

--- src/middleware/bind-emitters.ts
+++ src/middleware/bind-emitters.ts
@@ -1,12 +1,6 @@
-import { createNamespace, getRequestId } from "@emartech/cls-adapter";
+import { getRequestId } from "@emartech/cls-adapter";
 
 export const getBinderMW = () => async (ctx, next) => {
-  const namespace = createNamespace();
-
-  namespace.bindEmitter(ctx.req);
-  namespace.bindEmitter(ctx.res);
-
   ctx.state.requestId = getRequestId();
-
   await next();
 };
