237770baad6136467fdad4c70119edc0ec84386b
André Cruz <andre@cabine.org>
2018-12-01 01:10:27 +0000

    Converted update card

--- src/graphql/mutations/update-card.ts
+++ src/graphql/mutations/update-card.ts
@@ -1,14 +1,17 @@
-import { getRepository } from "typeorm";
-import { Card } from "../../entities/card";
+import { UserInputError } from "apollo-server-koa";
+import { Card } from "../../models/card";
 
 export const updateCardMutation = {
   async updateCard(obj, { id, patch }, context, info) {
-    const repository = getRepository(Card);
-    const card = await repository.findOne({ id });
-    const result = await repository.update(id, { ...patch });
-    return {
-      ...card,
-      ...patch,
-    };
+    const card = await Card.query().findOne("id", id);
+    if (!card) {
+      throw new UserInputError("Card not found");
+    }
+
+    const done = !card.done;
+    return Card.query()
+      .update(patch)
+      .returning("*")
+      .first();
   },
 };
