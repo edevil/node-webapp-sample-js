eaffda931d87a9b9c7542a9078d5cb06fc7409ef
André Cruz <andre@brpx.com>
2018-12-02 16:42:17 +0000

    Added missing attribute

--- src/migrations-new/20181202155111_initial_card.ts
+++ src/migrations-new/20181202155111_initial_card.ts
@@ -3,6 +3,7 @@ import * as Knex from "knex";
 exports.up = (knex: Knex): Promise<any> => {
   return Promise.all([
     knex.raw('create extension if not exists "uuid-ossp"'),
+    knex.raw('create extension if not exists "unaccent"'),
     knex.schema.createTable("card", table => {
       table
         .uuid("id")
@@ -14,11 +15,17 @@ exports.up = (knex: Knex): Promise<any> => {
         .boolean("done")
         .notNullable()
         .defaultTo(false);
+      table.specificType("searchVector", "tsvector");
+      table.index("searchVector", null, "gin");
       table.timestamps(true, true);
     }),
   ]);
 };
 
 exports.down = (knex: Knex): Promise<any> => {
-  return Promise.all([knex.schema.dropTable("card"), knex.raw('drop extension if exists "uuid-ossp"')]);
+  return Promise.all([
+    knex.schema.dropTable("card"),
+    knex.raw('drop extension if exists "uuid-ossp"'),
+    knex.raw('drop extension if exists "unaccent"'),
+  ]);
 };
