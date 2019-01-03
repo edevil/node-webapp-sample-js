732228ab2e004d663e7cbe1b022359f17f2eec99
André Cruz <andre@cabine.org>
2019-01-03 16:37:45 +0000

    WIP on asset pipeline

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const ManifestPlugin = require("webpack-manifest-plugin");
 
 module.exports = {
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
-  entry: ["@babel/polyfill", "./static/js/index.js"],
+  entry: ["@babel/polyfill", "./static/js/index.js", "./static/sass/app.scss"],
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: '[name].[contenthash].js',
-    publicPath: "/",
+    path: path.resolve(__dirname, "static/build"),
+    filename: "js/[name].[contenthash].js",
+    publicPath: "/static/",
   },
   module: {
     rules: [
@@ -21,6 +21,29 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "css/[name].[hash].css",
+            },
+          },
+          {
+            loader: "extract-loader",
+          },
+          {
+            loader: "css-loader?-url",
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
       },
     ],
   },
