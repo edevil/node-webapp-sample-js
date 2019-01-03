0c13a079c2818ae365de04cbbef90b59ec5ddc91
André Cruz <andre@cabine.org>
2019-01-03 17:44:26 +0000

    Work on template filter to access static assets

--- src/middleware/template-engine.js
+++ src/middleware/template-engine.js
@@ -1,20 +1,34 @@
 const appPath = require("app-root-path");
 const views = require("koa-views");
 const path = require("path");
+const { logger } = require("../logger");
+const nunjucks = require("nunjucks");
 
 const viewsPath = path.join(appPath.toString(), "/views");
 
-const getTemplateEngine = () =>
-  views(viewsPath, {
+const getTemplateEngine = () => {
+  let manifest;
+  try {
+    manifest = require("../../static/build/manifest");
+  } catch (err) {
+    logger.info(`Could not load asset manifest: ${err}`);
+    manifest = {};
+  }
+
+  const env = new nunjucks.Environment(new nunjucks.FileSystemLoader(viewsPath));
+  env.addFilter("shorten", function(str, count) {
+    return str.slice(0, count || 5);
+  });
+
+  return views(viewsPath, {
     map: {
       html: "nunjucks",
     },
     options: {
-      settings: {
-        views: viewsPath,
-      },
+      nunjucksEnv: env,
     },
   });
+};
 
 module.exports = {
   getTemplateEngine,
